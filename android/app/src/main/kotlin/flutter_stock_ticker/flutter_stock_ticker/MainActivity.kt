bfad3842a0cf49c37e96ac9ec26f81972b439819
Mziyanda Joko <joko.mzee@gmail.com>
2022-08-25 13:20:21 +0200

    adds data to ui

---
+++ android/app/src/main/kotlin/flutter_stock_ticker/flutter_stock_ticker/MainActivity.kt
@@ -0,0 +1,6 @@
+package flutter_stock_ticker.flutter_stock_ticker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'stock chart',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
